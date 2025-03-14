1ef0eba11443e024f34ef7016c026f9f391a2732
SysAdminDoc <54586742+SysAdminDoc@users.noreply.github.com>
2025-03-13 23:43:47 -0400

    Begin LibreSpotify

--- test.bat
+++
@@ -1,6 +0,0 @@
-:: Install Spicetify CLI
-winget install -e --id=Spicetify.Spicetify --silent --accept-source-agreements --accept-package-agreements --disable-interactivity
-
-:: Run Spicetify Marketplace installation as a non-admin user
-powershell -NoProfile -ExecutionPolicy Bypass -Command ^
-"Start-Process powershell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command Invoke-WebRequest -UseBasicParsing \"https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/install.ps1\" | Invoke-Expression; spicetify --bypass-admin apply' -NoNewWindow -Wait -Verb RunAs"
